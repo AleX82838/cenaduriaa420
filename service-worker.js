8adf603cc26101094310b0ce5dd13e5cca915c72
AleX82838 <alexanderfasa2806@gmail.com>
2025-09-28 12:57:52 -0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "cenaduria-v1";
+const CACHE_NAME = "cenaduria-v2";
 const urlsToCache = [
   "/",
   "/index.html",
@@ -18,3 +18,4 @@ self.addEventListener("fetch", event => {
     caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
+
